616a985d39e99cd428371364e482a28acb74b788
hbbtOfLight <kataryna.kazinskaya@gmail.com>
2021-10-03 13:40:00 +0300

    Add task_5_4

--- KatarynaKazinskaya/modules/legb.py
+++ KatarynaKazinskaya/modules/legb.py
@@ -5,6 +5,10 @@ def enclosing_funcion():
     a = "I am variable from enclosed function!"
 
     def inner_function():
-
-        a = "I am local variable!"
+        # a = "I am local variable!"   # next two lines replace it for 2, 3
+        # global a    # 2
+        nonlocal a    # 3
         print(a)
+    inner_function()   # 1
+
+
